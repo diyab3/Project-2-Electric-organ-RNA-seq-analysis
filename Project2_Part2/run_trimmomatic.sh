16f3ee1f221633b5ed18cc815554c5b996f7466e
Diya Balakrishnan <diyab@login1.talapas.uoregon.edu>
2026-09-01 18:50:04 -0700

    Adding run_trimmomatic.sh with the extra parameter that fixed my Java heap memory issue!

--- Project2_Part2/run_trimmomatic.sh
+++ Project2_Part2/run_trimmomatic.sh
@@ -7,7 +7,7 @@
 #SBATCH --error=LOG/trimDU_%j.err
 #SBATCH --time=01:00:00
 #SBATCH --cpus-per-task=16
-#SBATCH --mem=200G
+#SBATCH --mem=350G
 
 input_1="CcoxCrh_comrhy62_EO_6cm_1_1_cutadapt.fastq"
 
@@ -21,4 +21,4 @@ paired_output_2="CcoxCrh_comrhy62_EO_6cm_1_2_trimmed.fastq.gz"
 
 unpaired_output_2="CcoxCrh_comrhy62_EO_6cm_1_2_unpaired.fastq.gz"
 
-/usr/bin/time -v pixi run trimmomatic PE -threads 8 $input_1 $input_2 $paired_output_1 $unpaired_output_1 $paired_output_2 $unpaired_output_2 LEADING:3 TRAILING:3 SLIDINGWINDOW:5:15 MINLEN:35
+/usr/bin/time -v pixi run trimmomatic -Xmx16g PE -threads 16 $input_1 $input_2 $paired_output_1 $unpaired_output_1 $paired_output_2 $unpaired_output_2 LEADING:3 TRAILING:3 SLIDINGWINDOW:5:15 MINLEN:35
