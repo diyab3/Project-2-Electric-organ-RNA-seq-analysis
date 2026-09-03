49b6156b5272b4b87e8ba87d04c77774cab6db19
Diya Balakrishnan <diyab@login1.talapas.uoregon.edu>
2026-09-03 16:34:54 -0700

    Adding Project2_Part2 and all its new updates

---
+++ Project2_QAA/Project2_Part2/get_trimmed_read_length_distributions.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+#SBATCH --account=bgmp
+#SBATCH --partition=bgmp
+#SBATCH --job-name=getdist
+#SBATCH --output=LOG/getdistDU_%j.out
+#SBATCH --error=LOG/getdistDU_%j.err
+#SBATCH --time=01:00:00
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=350G
+
+infile="CcoxCrh_comrhy111_EO_adult_2_2_trimmed.fastq.gz"
+outfile="CcoxCrh_comrhy111_EO_adult_2_2_trimmed_read_length_dist.txt"
+
+/usr/bin/time -v zcat $infile | grep --no-group-separator -B 1 "+" | grep -v "+" | awk '{print(length($0))}' | sort| uniq -c |sort > $outfile
---
+++ Project2_QAA/Project2_Part2/run_trimmomatic.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+
+#SBATCH --account=bgmp
+#SBATCH --partition=bgmp
+#SBATCH --job-name=trim
+#SBATCH --output=LOG/trimDU_%j.out
+#SBATCH --error=LOG/trimDU_%j.err
+#SBATCH --time=01:00:00
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=350G
+
+input_1="CcoxCrh_comrhy62_EO_6cm_1_1_cutadapt.fastq"
+
+input_2="CcoxCrh_comrhy62_EO_6cm_1_2_cutadapt.fastq"
+
+paired_output_1="CcoxCrh_comrhy62_EO_6cm_1_1_trimmed.fastq.gz"  # where the reads that pass the filtering in R1 and R2 go
+
+unpaired_output_1="CcoxCrh_comrhy62_EO_6cm_1_1_unpaired.fastq.gz"  # where the reads that do not pass the filtering in both R1 and R2 go 
+
+paired_output_2="CcoxCrh_comrhy62_EO_6cm_1_2_trimmed.fastq.gz"
+
+unpaired_output_2="CcoxCrh_comrhy62_EO_6cm_1_2_unpaired.fastq.gz"
+
+/usr/bin/time -v pixi run trimmomatic -Xmx16g PE -threads 16 $input_1 $input_2 $paired_output_1 $unpaired_output_1 $paired_output_2 $unpaired_output_2 LEADING:3 TRAILING:3 SLIDINGWINDOW:5:15 MINLEN:35
