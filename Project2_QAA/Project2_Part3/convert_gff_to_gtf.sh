0e0337f27e899cc65d09b1ac61aa794d3819b722
Diya Balakrishnan <diyab@login1.talapas.uoregon.edu>
2026-09-02 17:08:17 -0700

    Updated convert_gff_to_gtf.sh to include variables instead of hard-coded filenames

--- Project2_QAA/Project2_Part3/convert_gff_to_gtf.sh
+++ Project2_QAA/Project2_Part3/convert_gff_to_gtf.sh
@@ -7,3 +7,8 @@
 #SBATCH --error=LOG/convertDU_%j.err
 #SBATCH --time=3:00:00
 #SBATCH --cpus-per-task=8
+
+gff="/projects/bgmp/shared/Bi623/Project2/campylomormyrus.gff"
+gtf_output="campylomormyrus.gtf"
+
+/usr/bin/time -v pixi run agat_convert_sp_gff2gtf.pl --gff $gff -o $gtf_output 
